3725821f952ced16f9c858a7e8d5c4ac8bc8a712
user28 <user28@no-reply.com>
2020-05-14 15:55:07 +0000

    Exercise 1

--- dg8-exercises/src/main/java/org/acme/Exercise1.java
+++ dg8-exercises/src/main/java/org/acme/Exercise1.java
@@ -9,16 +9,22 @@ public class Exercise1 {
 
     public static void main(String[] args) {
         // TODO: Construct a simple local cache manager with default configuration
+        DefaultCacheManager cacheManager = new DefaultCacheManager();
 
         // TODO: Define local cache configuration
+        cacheManager.defineConfiguration("local", new ConfigurationBuilder().build());
 
         // TODO: Obtain the local cache
+        Cache<String, String> cache = cacheManager.getCache("local");
 
         // TODO: Store a value
+        cache.put("name", "jEAN :-)");
 
         // TODO: Retrieve the value and print it out
+        System.out.printf("name = %s\n", cache.get("name"));
 
         // TODO: Stop the cache manager and release all resources
+        cacheManager.stop();
 
         System.exit(0);
 
